9ff7d6b6d70976de03cdabab33abc5a2d7ea27fa
Glenn Willen <gwillen@nerdnet.org>
2011-11-03 13:20:13 -0700

    true; type equality; heap lookup

--- lisp.templates.cc
+++ lisp.templates.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+struct True { void print() { printf("True"); } };
 struct Nil { void print() { printf("Nil"); } };
 template <typename car, typename cdr> struct Cons {};
 template <int n> struct Int {};
@@ -7,9 +8,21 @@ template <char name[80]> struct Sym {};
 template <int ctr> struct Gensym {};
 template <typename env, typename params, typename body> struct Func {};
 
+template <typename x, typename y> struct eq {
+  typedef Nil r_val;
+};
+template <typename x> struct eq<x, x> {
+  typedef True r_val;
+};
+
 template <int ctr> struct gensym {
   static const int r_val = ctr;
   static const int r_ctr = ctr + 1;
+};
+
+template <typename k, typename heap> struct heap_lookup {};
+template <typename k, typename v, typename rest> struct heap_lookup<k, Cons<Cons<k, v>, rest> > {
+  typedef v r_val;
 };
 
 template <typename exp, typename env, typename heap, int ctr> struct eval {};
@@ -24,7 +37,10 @@ char hello[] = "hello";
 
 int main() {
   eval<Nil, Nil, Nil, 0>::r_val a;
-  a.print();
+  //a.print();
+  eq<Cons<True, True>, Cons<True, True> >::r_val b;
+  b.print();
+  printf("\n");
 
   Sym<hello> x;
 }
